80b6c00846f54011c7b6731ff3611c59ab5424b0
DariaJoy <darya_joy@mail.ru>
2024-09-02 15:09:11 -0300

    lesson03

---
+++ lesson03/main.js
@@ -0,0 +1,6 @@
+let titleProject = "Оценка стоимости";
+let screensValue = "шаблонные, с уникальным дизайном, с анимациями";
+let screenPrice = 1000;
+let percentage = 50;
+let fullPrice = 5000;
+let responsive = true; 

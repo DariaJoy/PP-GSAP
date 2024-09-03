37fc1c25d4f0f42f634278d9bbc05eb4b9bfe650
DariaJoy <darya_joy@mail.ru>
2024-09-03 08:58:32 -0300

    lesson04.01

--- lesson04/main.js
+++ lesson04/main.js
@@ -1,10 +1,10 @@
-let titleProject = promt("Название проекта?");
-let screensValue = promt("Типы экранов: шаблонные, с уникальным дизайном, с анимациями?");
+let titleProject = prompt("Название проекта?");
+let screensValue = prompt("Типы экранов: шаблонные, с уникальным дизайном, с анимациями?");
 let responsive = confirm("Hужен ли респонсивный сайт?");
-let service1 = promt("Какая услуга или сервис Вам нужен?");
-let servicePrice1 = +promt("Сколько Вы ожидаете затратить на выполнение услуги?");
+let service1 = prompt("Какая услуга или сервис Вам нужен?");
+let servicePrice1 = +prompt("Сколько Вы ожидаете затратить на выполнение услуги?");
 let service2 = confirm("Вы планируете заказать ещё услугу или сервис?");
-let servicePrice2 = +promt("Сколько Вы ожидаете затратить на выполнение второй услуги?");
+let servicePrice2 = +prompt("Сколько Вы ожидаете затратить на выполнение второй услуги?");
 let fullPrice = servicePrice1 + servicePrice2;
 let percent = Math.round(fullPrice * 0.10);
 let servicePercentPrice  = Math.round(fullPrice - percent);
@@ -15,8 +15,6 @@ if (fullPrice > 50000) {
     console.log("Сделаем скидку в 10%");
 } else if (fullPrice > 20000 && fullPrice < 50000) {
     console.log("Сделаем скидку 5%");
-} else if (fullPrice === 20000 || fullPrice === 50000) {
-    console.log("Ровная сумма, что будем делать?");
 } else if (fullPrice > 0 && fullPrice < 20000) {
     console.log("Скидка не предусмотрена");
 } else if (fullPrice < 0) {
